61645608db187f26893932b34c17c4a919ebf82a
IcaroOm <icaroomdigital@gmail.com>
2024-07-13 17:02:25 -0300

    Add sql database for generated passwords, updating UI using htmx and tailwind WIP

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
-from fastapi import FastAPI, APIRouter, HTTPException, Request, Form
+from fastapi import FastAPI, APIRouter, HTTPException, Request, Form, Header
 from fastapi.responses import HTMLResponse, JSONResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from pydantic import BaseModel
+from sqlalchemy import select
+from typing import Optional
 import random
 import string
 import requests
-
+from models import database, passwords
 
 app = FastAPI()
 
 router = APIRouter()
-
-app.mount("/static", StaticFiles(directory="static"), name="static")
 
 templates = Jinja2Templates(directory="templates")
 
@@ -41,6 +42,25 @@ async def generate_password(request: Request, length: int):
     return {"password": password}
 
 
+class SavePasswordRequest(BaseModel):
+    name: str
+    password: str
+
+
+@router.post("/save_password", response_class=JSONResponse)
+async def save_password(request: SavePasswordRequest):
+    query = passwords.insert().values(password=request.password, length=len(request.password), name=request.name)
+    await database.execute(query)
+    return {"message": "Password saved successfully"}
+
+
+@router.get("/stored_passwords", response_class=HTMLResponse)
+async def stored_passwords(request: Request):
+    query = select(passwords.c.name, passwords.c.password, passwords.c.length, passwords.c.created_at)
+    saved_passwords = await database.fetch_all(query)
+    return templates.TemplateResponse("stored_passwords.html", {"request": request, "passwords": saved_passwords})
+
+
 def check_pwned_email(email: str) -> dict:
     response = requests.get(f'https://leakcheck.io/api/public?check={email}')
     if response.status_code != 200:
@@ -54,7 +74,7 @@ async def check_email_get(request: Request):
 
 
 @router.post("/check_email", response_class=HTMLResponse)
-async def check_email_post(request: Request, email: str = Form(...)):
+async def check_email_post(request: Request, email: str = Form(...), hx_request: Optional[str] = Header(None)):
     data = check_pwned_email(email)
     if data['found'] == 0:
         message = 'Your email has not been found in any data breaches.'
@@ -62,7 +82,26 @@ async def check_email_post(request: Request, email: str = Form(...)):
     else:
         message = f'Your email has been found in {data["found"]} data breaches.'
         sources = data['sources']
+    if hx_request:
+        return templates.TemplateResponse("table_sources.html",  {"request": request, "message": message, "sources": sources, "email": email})
     return templates.TemplateResponse("email_check.html", {"request": request, "message": message, "sources": sources, "email": email})
 
 
+@router.get("/stored_passwords", response_class=HTMLResponse)
+async def stored_passwords(request: Request):
+    query = passwords.select()
+    saved_passwords = await database.fetch_all(query)
+    return templates.TemplateResponse("stored_passwords.html", {"request": request, "passwords": saved_passwords})
+
+
 app.include_router(router)
+
+
+@app.on_event("startup")
+async def startup():
+    await database.connect()
+
+
+@app.on_event("shutdown")
+async def shutdown():
+    await database.disconnect()
---
+++ models.py
@@ -0,0 +1,20 @@
+from databases import Database
+import sqlalchemy
+
+DATABASE_URL = "sqlite:///./passwords.db"
+
+database = Database(DATABASE_URL)
+metadata = sqlalchemy.MetaData()
+
+passwords = sqlalchemy.Table(
+    "passwords",
+    metadata,
+    sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
+    sqlalchemy.Column("password", sqlalchemy.String, nullable=False),
+    sqlalchemy.Column("length", sqlalchemy.Integer, nullable=False),
+    sqlalchemy.Column("name", sqlalchemy.String, nullable=False),
+    sqlalchemy.Column("created_at", sqlalchemy.DateTime, server_default=sqlalchemy.func.now()),
+)
+
+engine = sqlalchemy.create_engine(DATABASE_URL)
+metadata.create_all(engine)
